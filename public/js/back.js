8f439ea1d9cae557a29e4a795c669022bfc4303f
Pierluigi La Rocca <laroccapierluigi1998@gmail.com>
2022-04-08 19:44:44 +0200

    Tag controller completato

--- public/js/back.js
+++ public/js/back.js
@@ -94,7 +94,7 @@
 /***/ (function(module, exports) {
 
 //eliminazione dalla pagina
-window.btnDelete = btnDelete; //funzione dalla quale ricavo l'id e poi lo passo nell'action del form dell'index
+window.btnDelete = btnDelete; //funzione dalla quale ricavo l'id e la rotta poi lo passo nell'action del form dell'index
 
 function btnDelete(id, route) {
   document.getElementById("myForm").action = route + "/" + id;
--- resources/js/back.js
+++ resources/js/back.js
@@ -1,7 +1,7 @@
 //eliminazione dalla pagina
 window.btnDelete= btnDelete;
 
-//funzione dalla quale ricavo l'id e poi lo passo nell'action del form dell'index
+//funzione dalla quale ricavo l'id e la rotta poi lo passo nell'action del form dell'index
 function btnDelete(id,route){
   document.getElementById("myForm").action = route + "/" + id;
 }
